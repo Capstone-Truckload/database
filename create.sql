f6a6a6055c035870e24646c3e3e5031ac0279848
Heath Richardson <mheathr@gmail.com>
2018-09-03 10:42:23 -0500

    sql to create database, tables

---
+++ create.sql
@@ -0,0 +1,39 @@
+create database capstone;
+
+create table Customer (
+       id bigserial,
+       phone varchar,
+       email varchar,
+       pass varchar,
+       payment money,
+       cName varchar, /* name is a non-reserved keyword */
+       PRIMARY KEY(id)
+);
+
+create table Trucker (
+       id bigserial,
+       phone varchar,
+       email varchar,
+       password varchar,
+       licensing varchar,
+       capacity varchar,
+       tName varchar,
+       PRIMARY KEY(id)
+);
+
+create table oads (
+       id bigserial,
+       customer_id bigserial REFERENCES  Customer (id),
+       trucker_id bigserial REFERENCES Trucker(id),
+       destination_to varchar,
+       destination_from varchar,
+       datePosted date,
+       dateRequired date,
+       price money,
+       licensingRequirement varchar,
+       requirements varchar,
+       comments varchar,
+       currentPrice money,
+       expirationDate date,
+       bigEndDate date
+);
